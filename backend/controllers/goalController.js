7211add40a51d5d78ea633e815857b372ad8bf70
kipkoech88 <hoseakipkoech88@gmail.com>
2023-06-27 23:45:00 +0300

    added delete functionality to db

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -35,7 +35,16 @@ const updateGoals = asyncHandler(async(req, res)=>{
 })
 
 const deleteGoals = asyncHandler(async(req, res)=>{
-    res.status(200).json({Message: `Delete goals ${id}`})
+
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal){
+        res.status(400)
+        throw new Error("Goal not found")
+    }
+
+    await goal.remove()
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {getGoals, 
