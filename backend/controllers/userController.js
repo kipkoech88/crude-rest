1c99c940b0d47676779168529329906453773e08
kipkoech88 <hoseakipkoech88@gmail.com>
2023-06-28 17:44:57 +0300

    Added user login, register and getuser data functions in the user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,30 @@
+//@desc Register user
+//@route api/users
+//@access private
+
 const registerUser = (req, res)=>{
     res.json({message: "Register USers"})
 }
 
+//@desc Authenticate user
+//@route POST/api/users/login
+//@access Public
+
+const loginUser = (req, res)=>{
+    res.json({message: "Login USers"})
+}
+
+//@desc Get users data
+//@route GET/api/users/me
+//@access Public
+
+
+const getUser = (req, res)=>{
+    res.json({message: "Register USers"})
+}
+
 module.exports = {
+    loginUser,
     registerUser,
+    getUser,
 }
