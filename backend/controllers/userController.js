5cabd23f75eefeffb02789f56ecd8876e276f99a
kipkoech88 <hoseakipkoech88@gmail.com>
2023-06-30 15:55:49 +0300

    added generate token to user controller, registerUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,7 +38,8 @@ const registerUser = asyncHandler(async(req, res)=>{
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     }
     else{
@@ -81,6 +82,14 @@ const loginUser = asyncHandler(async(req, res)=>{
 const getUser = asyncHandler(async(req, res)=>{
     res.json({message: "Register USers"})
 })
+
+
+//Generate JWT
+const generateToken = (id)=>{
+    return jwt.sign(id, process.env.JWT_SECRET, {
+        expiresIn: "30d"
+    })
+}
 
 module.exports = {
     loginUser,
