6cb9a28d8dcc70c1188a13f3eb9d36dc9d664867
kipkoech88 <hoseakipkoech88@gmail.com>
2023-06-30 15:41:50 +0300

    user registration logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,6 +8,43 @@ const User = require("../models/userModel");
 //@access private
 
 const registerUser = asyncHandler(async(req, res)=>{
+    const {email, name, password } = req.body;
+
+    if (!email || !name || !password){
+        res.status(400);
+        throw new Error("Please fill all the fields")
+    }
+
+    //See if usr exists
+    const userExists = await User.findOne({email});
+
+    if (userExists){
+        res.status(400);
+        throw new Error("User already exists")
+    }
+
+    //Hash passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //create users
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user){
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    }
+    else{
+        res.status(400);
+        throw new Error("Unable to create user")
+    }
     res.json({message: "Register USers"})
 })
 
