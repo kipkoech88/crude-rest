98109910e6cc5ac571cc9036fe4b53962ab24f69
kipkoech88 <hoseakipkoech88@gmail.com>
2023-06-30 16:40:39 +0300

    access to goals of a specific user

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler")
 const Goal = require("../models/goalModel")
 
 const getGoals = asyncHandler(async(req, res)=>{
-    const goal = await Goal.find()
+    const goal = await Goal.find({user: req.user.id})
 
     res.status(200).json(goal)
 })
