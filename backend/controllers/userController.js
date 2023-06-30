63cd18394b63265661b9e3f089cacc240b9bce1a
kipkoech88 <hoseakipkoech88@gmail.com>
2023-06-30 15:27:47 +0300

    made the user controller async

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,26 +7,26 @@ const User = require("../models/userModel");
 //@route api/users
 //@access private
 
-const registerUser = (req, res)=>{
+const registerUser = asyncHandler(async(req, res)=>{
     res.json({message: "Register USers"})
-}
+})
 
 //@desc Authenticate user
 //@route POST/api/users/login
 //@access Public
 
-const loginUser = (req, res)=>{
+const loginUser = asyncHandler(async(req, res)=>{
     res.json({message: "Login USers"})
-}
+})
 
 //@desc Get users data
 //@route GET/api/users/me
 //@access Public
 
 
-const getUser = (req, res)=>{
+const getUser = asyncHandler(async(req, res)=>{
     res.json({message: "Register USers"})
-}
+})
 
 module.exports = {
     loginUser,
